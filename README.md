916c22d5d56dfb0d0c00cc86c5e44e3b177b9dc1
blangwell <b.langwell@outlook.com>
2020-10-26 13:48:42 -0700

    [index] Create new audio objects, add function playSound(), add DOMContentLoaded listener

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Tooting Monk
-An immature homage to the Shockwave/Flash browser distractions of the late nineties and early aughts.
+An immature homage to the Shockwave/Flash browser distractions of the late nineties and early aughts. Inspired in part by [His Holiness the Dalai Lama](https://www.upworthy.com/the-dalai-lama-has-some-very-uplifting-and-wise-words-about-farting)
 
 ## Technologies
 - HTML5
---
+++ index.js
@@ -0,0 +1,32 @@
+const monk = document.getElementById('monk')
+
+const toot1 = new Audio('./assets/toot1.mp3'),
+toot2 = new Audio('./assets/toot2.mp3'),
+toot3 = new Audio('./assets/toot3.mp3'),
+toot4 = new Audio('./assets/toot4.mp3'),
+toot5 = new Audio('./assets/toot5.mp3'),
+toot6 = new Audio('./assets/toot6.mp3'),
+toot7 = new Audio('./assets/toot7.mp3'),
+toot8 = new Audio('./assets/toot8.mp3'),
+toot9 = new Audio('./assets/toot9.mp3'),
+toot10 = new Audio('./assets/toot10.mp3');
+
+const tootArray = [toot1, toot2, toot3, toot4, toot5, toot6, toot7, toot8, toot9, toot10]
+
+// let tootIndex = Math.floor(Math.random() * 10)
+
+
+const tootAccident1 = './assets/toot_accident1.mp3',
+tootAccident2 = './assets/toot_accident2.mp3';
+
+
+function playSound() {
+  console.log('play sound')
+  let tootIndex = Math.floor(Math.random() * 10);
+  let randomToot = tootArray[tootIndex];
+  randomToot.play();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  monk.addEventListener('click', playSound)
+})
