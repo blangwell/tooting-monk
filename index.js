e27ad15670f011d868d3488982446c249921c50d
blangwell <b.langwell@outlook.com>
2020-10-26 17:13:33 -0700

    [index] Remove duplicate sound file, remove accidentArray and accidentIndex

--- index.js
+++ index.js
@@ -11,23 +11,18 @@ toot8 = new Audio('./assets/sounds/toot8.mp3'),
 toot9 = new Audio('./assets/sounds/toot9.mp3'),
 toot10 = new Audio('./assets/sounds/toot10.mp3');
 
-const tootAccident1 = new Audio('./assets/sounds/toot_accident1.mp3'),
-tootAccident2 = new Audio('./assets/sounds/toot_accident2.mp3');
+const tootAccident = new Audio('./assets/sounds/toot_accident1.mp3');
 
 const tootArray = [toot1, toot2, toot3, toot4, toot5, toot6, toot7, toot8, toot9, toot10];
-const accidentArray = [tootAccident1, tootAccident2];
 
 let count = 0;
 
 function playSound() {
   let tootIndex = Math.floor(Math.random() * 10);
-  let accidentIndex = Math.floor(Math.random() * 2);
   let countMax = Math.floor(Math.random() * 10) + 5
-  
   let randomToot = tootArray[tootIndex];
-  let randomAccident = accidentArray[accidentIndex];
 
-  console.log(`count: ${count} countMax: ${countMax} tootIndex: ${tootIndex} accidentIndex: ${accidentIndex}`)
+  console.log(`count: ${count} countMax: ${countMax} tootIndex: ${tootIndex}`)
 
   if (count < countMax) {
     monk.classList.add('tooting');
@@ -39,7 +34,7 @@ function playSound() {
 
   } else {
     monk.classList.add('accident');
-    randomAccident.play();
+    tootAccident.play();
     count = 0;
     setTimeout(() => {
       monk.classList.remove('accident')
