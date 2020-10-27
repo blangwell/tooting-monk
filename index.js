6fc7d0170fe51cd97751e41bf57898c0e170ccc0
blangwell <b.langwell@outlook.com>
2020-10-26 17:06:27 -0700

    [style] Add background image asset and refactor css

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const monk = document.querySelector('.monk');
 
-const toot1 = new Audio('./assets/toot1.mp3'),
-toot2 = new Audio('./assets/toot2.mp3'),
-toot3 = new Audio('./assets/toot3.mp3'),
-toot4 = new Audio('./assets/toot4.mp3'),
-toot5 = new Audio('./assets/toot5.mp3'),
-toot6 = new Audio('./assets/toot6.mp3'),
-toot7 = new Audio('./assets/toot7.mp3'),
-toot8 = new Audio('./assets/toot8.mp3'),
-toot9 = new Audio('./assets/toot9.mp3'),
-toot10 = new Audio('./assets/toot10.mp3');
+const toot1 = new Audio('./assets/sounds/toot1.mp3'),
+toot2 = new Audio('./assets/sounds/toot2.mp3'),
+toot3 = new Audio('./assets/sounds/toot3.mp3'),
+toot4 = new Audio('./assets/sounds/toot4.mp3'),
+toot5 = new Audio('./assets/sounds/toot5.mp3'),
+toot6 = new Audio('./assets/sounds/toot6.mp3'),
+toot7 = new Audio('./assets/sounds/toot7.mp3'),
+toot8 = new Audio('./assets/sounds/toot8.mp3'),
+toot9 = new Audio('./assets/sounds/toot9.mp3'),
+toot10 = new Audio('./assets/sounds/toot10.mp3');
 
-const tootAccident1 = new Audio('./assets/toot_accident1.mp3'),
-tootAccident2 = new Audio('./assets/toot_accident2.mp3');
+const tootAccident1 = new Audio('./assets/sounds/toot_accident1.mp3'),
+tootAccident2 = new Audio('./assets/sounds/toot_accident2.mp3');
 
 const tootArray = [toot1, toot2, toot3, toot4, toot5, toot6, toot7, toot8, toot9, toot10];
 const accidentArray = [tootAccident1, tootAccident2];
